a5a4a8eec37eded77e1e5f1a24effde80830a048
EK Nayeem <ebrahim.khalil.nayeem@gmail.com>
2025-09-30 19:14:17 +0600

    feat(api): better auth

--- packages/api-client/src/index.ts
+++ packages/api-client/src/index.ts
@@ -5,7 +5,7 @@ import { hc } from "hono/client";
 // create instance to inline type in build
 // https://hono.dev/docs/guides/rpc#compile-your-code-before-using-it-recommended
 // eslint-disable-next-line unused-imports/no-unused-vars
-const client = hc<router>("");
+const client = hc<router>("", { init: { credentials: "include" } });
 export type Client = typeof client;
 
 export default (...args: Parameters<typeof hc>): Client =>
